b3af04c22c1d205d2598e277884661303cb53954
Aslanbek Astamirov <asmmmmir@gmail.com>
2023-06-07 21:18:51 +0500

    some update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import Navigation from "./components/Header/Navigation";
-import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Main from "./components/Main/Main";
 import Project from "./pages/Project";
+import Projects from "./components/Projects/Projects";
 
 function App() {
   return (
     <BrowserRouter>
       <Navigation />
       <Routes>
-        <Route index path="/my-portfolio" Component={Main}/>
+        <Route path="/my-portfolio" Component={Main} />
         <Route path="/my-portfolio/project/:id" Component={Project} />
       </Routes>
-
       <Footer />
     </BrowserRouter>
   );
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -9,27 +9,27 @@ const Projects = () => {
 
             <ul className={styles.projects__lists}>
                 <Link to='/my-portfolio/project/1' className={styles.projects__item}>
-                    <img src="./my-portfolio/img/01.jpg" alt="" />
+                    <img src="../my-portfolio/img/01.jpg" alt="" />
                     <h3 className={styles.projects__name}>Movies App</h3>
                 </Link>
                 <Link to='/my-portfolio/project/2' className={styles.projects__item}>
-                    <img src='./my-portfolio/img/02.jpg' alt="" />
+                    <img src='../my-portfolio/img/02.jpg' alt="" />
                     <h3 className={styles.projects__name}>Fast food shop</h3>
                 </Link>
                 <Link to='/my-portfolio/project/3' className={styles.projects__item}>
-                    <img src='./my-portfolio/img/03.jpg' alt=""/>
+                    <img src='../my-portfolio/img/03.jpg' alt=""/>
                     <h3 className={styles.projects__name}>Video portal</h3>
                 </Link>
                 <Link to='/my-portfolio/project/4' className={styles.projects__item}>
-                    <img src='./my-portfolio/img/04.jpg' alt="" />
+                    <img src='../my-portfolio/img/04.jpg' alt="" />
                     <h3 className={styles.projects__name}>Dating app</h3>
                 </Link>
                 <Link to='/my-portfolio/project/5' className={styles.projects__item}>
-                    <img src='./my-portfolio/img/05.jpg' alt="" />
+                    <img src='../my-portfolio/img/05.jpg' alt="" />
                     <h3 className={styles.projects__name}>Landing</h3>
                 </Link>
                 <Link to='/my-portfolio/project/6' className={styles.projects__item}>
-                    <img src='./my-portfolio/img/06.jpg' alt="" />
+                    <img src='../my-portfolio/img/06.jpg' alt="" />
                     <h3 className={styles.projects__name}>Gaming community</h3>
                 </Link>
             </ul>
