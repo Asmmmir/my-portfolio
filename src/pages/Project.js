bcb4c1731e685839da06adb262831bc031e8a45a
Aslanbek <107951255+Asmmmir@users.noreply.github.com>
2024-11-12 21:29:51 +0500

    Update Project.js

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -17,11 +17,11 @@ export default function Project() {
 
     const projects = {
         
-        1: {name: 'Instagram Clone', image: '../img/instagram.jpg',stack:'React, Chakra UI, React Router', view: 'https://asmmmir.github.io/instagram-clone/', github: 'https://github.com/Asmmmir/instagram-clone' },
+        // 1: {name: 'Instagram Clone', image: '../img/instagram.jpg',stack:'React, Chakra UI, React Router', view: 'https://asmmmir.github.io/instagram-clone/', github: 'https://github.com/Asmmmir/instagram-clone' },
         2: {name: 'Nike', image: '../img/nike.png',stack:'React, Tailwind', view: 'https://asmmmir.github.io/nike_landing/', github: 'https://github.com/Asmmmir/nike_landing' },
         3: {name: 'Gericht Restaurant', image: '../img/gericht.jpg',stack:'React, Tailwind', view: 'https://asmmmir.github.io/gericht-restaurant/', github: 'https://github.com/Asmmmir/gericht-restaurant' },
         4: {name: 'Good Food', image: '../img/goodfood.jpg',stack:'React, SCSS, React icons', view: 'https://asmmmir.github.io/good-food', github: 'https://github.com/Asmmmir/good-food' },
-        5: {name: 'Typing speed', image: '../img/typeapp.png',stack:'HTML, CSS, JavaScript', view: 'https://asmmmir.github.io/typeapp/', github: 'https://github.com/Asmmmir/typeapp' },
+        // 5: {name: 'Typing speed', image: '../img/typeapp.png',stack:'HTML, CSS, JavaScript', view: 'https://asmmmir.github.io/typeapp/', github: 'https://github.com/Asmmmir/typeapp' },
         6: {name: 'Kropp', image: '../img/kropp.png',stack:'HTML, CSS', view: 'https://asmmmir.github.io/kropp_crossfit/', github: 'https://github.com/Asmmmir/kropp_crossfit' },
 
     }
